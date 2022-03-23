5ab165f85fc9fba507188aa9537caa5c5b897c4e
EpicJokinFail <joaquin.pb@hotmail.es>
2022-03-23 20:01:49 +0100

    2022-03-23 Añadido apartado backend para el proyecto

---
+++ backend/src/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use( express.json());
+
+app.get('/api/tierList', (req, res) =>{
+    let status = 200;
+    let tierLists = [
+        {
+            id: 1,
+            name: 'A',
+            desc: 'DescA',
+            tiers: []
+        },
+        {
+            id: 2,
+            name: 'B',
+            desc: 'DescB',
+            tiers: []
+        },
+    ];
+    res.status(status).send(tierLists);
+});
+
+app.get('/api/tierList/:id', (req, res) =>{
+    let status = 200;
+    let { id } = req.params;
+    let tier = {
+        id: id,
+        name: 'A',
+        desc: 'DescA',
+        tiers: []
+    };
+    res.status(status).send(tier);
+})
+
+
+app.listen(PORT, () => {
+    console.log(`listening on port -> ${PORT}`);
+})
